bd5891ce870d1a8e18e95e7768882f01ca75ab47
Benji Stansfield <benji.stansfield@students.snow.edu>
2025-11-11 17:08:29 -0700

    CircleFactory created

---
+++ Lab10.Main/CircleFactory.cs
@@ -0,0 +1,20 @@
+namespace Lab10;
+
+public class CircleFactory : IGraphic2DFactory
+{
+    public string Name => "Circle";
+
+    public IGraphic2D Create()
+    {
+        Console.Write("X Coordinate: ");
+        int x = int.Parse(Console.ReadLine());
+
+        Console.Write("Y Coordinate: ");
+        int y = int.Parse(Console.ReadLine());
+
+        Console.Write("Radius: ");
+        int radius = int.Parse(Console.ReadLine());
+
+        return new CircleFactory(x, y, radius);
+    }
+}
